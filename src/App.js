e5b01b39a543ae34ca04259d37a49e9694296cc9
Dav991123 <sargsyand89@gmail.com>
2024-06-23 14:13:16 +0400

    x

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import { useEffect } from 'react';
+import OneSignal from 'react-onesignal';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  useEffect(() => {
+    OneSignal.init({
+      appId: "75f26761-cd6f-457d-a5ef-a482853c089a",
+      allowLocalhostAsSecureOrigin: true,
+      apiKey: 'OWRkOTc4YzgtZjM5NS00MGU2LWFiNDMtNWMxMWZkNmEyNzJj', 
+    });
+  }, []);
+ 
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
